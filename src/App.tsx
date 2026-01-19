ddac8fe0eb314ac96cca9bd942aa3af2ef14083d
giap <giaptapcode.dev@gmail.com>
2026-01-19 10:15:49 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen w-full flex items-center justify-center">
+        <h1 className="font-bold text-5xl flex items-center justify-center">Tripdi x NgocGiap</h1>
+      </div >
+    </>
+  )
+}
+
+export default App
